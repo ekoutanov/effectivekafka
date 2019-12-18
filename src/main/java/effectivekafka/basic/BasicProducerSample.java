708228999eda2bcb6ca5ead80e2f23a90f2e4eb3
Emil Koutanov <ekoutanov@gmail.com>
2019-12-18 20:58:49 +1100

    Initial commit

---
+++ src/main/java/effectivekafka/basic/BasicProducerSample.java
@@ -0,0 +1,25 @@
+package effectivekafka.basic;
+
+import java.util.*;
+
+import org.apache.kafka.clients.*;
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.*;
+
+public final class BasicProducerSample {
+  public static void main(String[] args) throws InterruptedException {
+    Map<String, Object> config = Map.of(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092", 
+                                        ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName(), 
+                                        ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName(), 
+                                        ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, 1);
+    
+    try (Producer<String, String> producer = new KafkaProducer<>(config)) {
+      while (true) {
+        final String value = new Date().toString();
+        System.out.format("Publishing record with value %s%n", value);
+        producer.send(new ProducerRecord<>("test", "myKey", value));
+        Thread.sleep(1000);
+      }
+    }
+  }
+}
