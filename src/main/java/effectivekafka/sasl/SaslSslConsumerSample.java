7488a4d53f6e2825b93ec31927830a0d84d4a8e2
Emil Koutanov <ekoutanov@gmail.com>
2020-02-21 22:18:52 +1100

    SASL_SSL examples

---
+++ src/main/java/effectivekafka/sasl/SaslSslConsumerSample.java
@@ -0,0 +1,50 @@
+package effectivekafka.sasl;
+
+import static java.lang.System.*;
+
+import java.time.*;
+import java.util.*;
+
+import org.apache.kafka.clients.*;
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.common.config.*;
+import org.apache.kafka.common.security.scram.*;
+import org.apache.kafka.common.serialization.*;
+
+public final class SaslSslConsumerSample {
+  public static void main(String[] args) {
+    final var topic = "getting-started";
+
+    final var loginModuleClass = ScramLoginModule.class.getName();
+    final var saslJaasConfig = loginModuleClass 
+        + " required\n"
+        + "username=\"alice\"\n"
+        + "password=\"alice-secret\";";
+
+    final var config = new HashMap<String, Object>();
+    config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9093");
+    config.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SSL");
+    config.put(SslConfigs.SSL_ENDPOINT_IDENTIFICATION_ALGORITHM_CONFIG, "https");
+    config.put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, "client.truststore.jks");
+    config.put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, "secret");
+    config.put(SaslConfigs.SASL_MECHANISM, "SCRAM-SHA-512");
+    config.put(SaslConfigs.SASL_JAAS_CONFIG, saslJaasConfig);
+    config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+    config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName()); 
+    config.put(ConsumerConfig.GROUP_ID_CONFIG, "basic-consumer-sample");
+    config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+    config.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
+
+    try (var consumer = new KafkaConsumer<String, String>(config)) {
+      consumer.subscribe(Set.of(topic));
+
+      while (true) {
+        final var records = consumer.poll(Duration.ofMillis(100));
+        for (var record : records) {
+          out.format("Got record with value %s%n", record.value());
+        }
+        consumer.commitAsync();
+      }
+    }
+  }
+}
---
+++ src/main/java/effectivekafka/sasl/SaslSslProducerSample.java
@@ -0,0 +1,55 @@
+package effectivekafka.sasl;
+
+import static java.lang.System.*;
+
+import java.util.*;
+
+import org.apache.kafka.clients.*;
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.config.*;
+import org.apache.kafka.common.security.scram.*;
+import org.apache.kafka.common.serialization.*;
+
+public final class SaslSslProducerSample {
+  public static void main(String[] args) throws InterruptedException {
+    final var topic = "getting-started";
+
+    final var loginModuleClass = ScramLoginModule.class.getName();
+    final var saslJaasConfig = loginModuleClass 
+        + " required\n"
+        + "username=\"alice\"\n"
+        + "password=\"alice-secret\";";
+    
+    final Map<String, Object> config = 
+        Map.of(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9094", 
+               CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SASL_SSL",
+               SslConfigs.SSL_ENDPOINT_IDENTIFICATION_ALGORITHM_CONFIG, "https",
+               SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, "client.truststore.jks",
+               SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, "secret",
+               SaslConfigs.SASL_MECHANISM, "SCRAM-SHA-512",
+               SaslConfigs.SASL_JAAS_CONFIG, saslJaasConfig,
+               ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName(), 
+               ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName(), 
+               ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, true);
+
+    try (var producer = new KafkaProducer<String, String>(config)) {
+      while (true) {
+        final var key = "myKey";
+        final var value = new Date().toString();
+        out.format("Publishing record with value %s%n", 
+                   value);
+
+        final Callback callback = (metadata, exception) -> {
+          out.format("Published with metadata: %s, error: %s%n", 
+                     metadata, exception);
+        };
+
+        // publish the record, handling the metadata in the callback
+        producer.send(new ProducerRecord<>(topic, key, value), callback);
+
+        // wait a second before publishing another
+        Thread.sleep(1000);
+      }
+    }
+  }
+}
